b2463b1e8fd3362d49e17b48e67ea0700653f464
Pro5507 <nhupendrakumar9162@gmail.com>
2024-11-01 11:26:53 +0530

    List&Dictonary

---
+++ dict.py
@@ -0,0 +1,6 @@
+num= {"odd": 5 , "even": 7}
+print(num)
+if num%2==0:
+    print("Both are Even")
+else:
+    print("Both are odd")
---
+++ list.py
@@ -0,0 +1,3 @@
+num= [1,6,4,3,9,25,33,8,90,7]
+even= [x for x in num if x%2==0]
+print(even)
